8cef9175c1cea1819c5dec77e4a8763cf8956561
RufixDuke <rufixduke01@gmail.com>
2022-08-11 09:56:08 +0100

    Tried using the Sofia Pro font but cant use it because I do not have android studio and I cant use emulator with expo

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import MyDrawer from "./navigator";
+// import AppLoading from "expo-app-loading";
+// import { useFonts } from "expo-font";
 
 export default function App() {
+    // let [fontsLoaded] = useFonts({
+    //     Sofia: require("./assets/font/sofiapro-light.otf"),
+    // });
+
+    // if (!fontsLoaded) {
+    //     return <AppLoading />;
+    // }
+
     return <MyDrawer />;
 }
